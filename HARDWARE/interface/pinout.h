160b5b35ea8593942fa2ab7389209359a026a515
Suvian <suvian@sjtu.edu.cn>
2020-07-21 14:18:29 +0800

    修改ADC采集输入管脚

--- HARDWARE/interface/pinout.h
+++ HARDWARE/interface/pinout.h
@@ -18,7 +18,7 @@
 #define UART_TX_PIN 		31
 #define UART_RTS_PIN 		26
 
-#define AIN_VBAT 			ADC_CONFIG_PSEL_AnalogInput4	/*电压采集引脚*/
+#define AIN_VBAT 			ADC_CONFIG_PSEL_AnalogInput5	/*电压采集引脚*/
 #define AIN_VBAT_DIVIDER 	ADC_CONFIG_INPSEL_AnalogInputOneThirdPrescaling	/*模拟输入1/3分压*/
 #define ADC_SCALER 			(2.0/1.0)	/*电阻分压*/
 #define ADC_DIVIDER 		(3.3/1.0)	/*电压缩放比例*/
